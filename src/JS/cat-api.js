c2784e90eed7f8cf2d7ba4422fa5bbfe2c958468
Iryna220 <ibiriszka951@gmail.com>
2023-07-01 14:02:52 +0200

    update home work 10

--- src/JS/cat-api.js
+++ src/JS/cat-api.js
@@ -1,23 +1,25 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio'
+
 const BASE_URL = "https://api.thecatapi.com/v1/";
 const API_KEY = "live_u21Y5Igzf6bc5bGcRoQ2WJn4RVCcVNRckcdQ4GPdVUddRpaapl6Wd4Ffnyg1db3t";
 
 export const fetchBreeds = function() {
-    return fetch(`${BASE_URL}breeds?api_key=${API_KEY}`).
-        then(resp => {
-        if (!resp.ok) {
+    return fetch(`${BASE_URL}breeds?api_key=${API_KEY}`)
+        .then(response => {
+        if (!response.ok) {
          Notify.failure("Oops! Something went wrong! Try reloading the page!")
         }
-        return resp.json()
+        return response.json()
  })   
 }
 
 export const fetchCatByBreed = function(breedId) {
-    return fetch(`${BASE_URL}images/search?&breed_ids=${breedId}&api_key=${API_KEY}`)
-        .then(resp => {
-            if (!resp.ok) {
+    return fetch(`${BASE_URL}images/search?breed_ids=${breedId}&api_key=${API_KEY}`)
+        .then(response => {
+            if (!response.ok) {
                 Notify.failure("Oops! Something went wrong! Try reloading the page!")
             }
-            return resp.json()
+            return response.json()
         })
         .catch(err => console.log(err));
 }
--- src/JS/index.js
+++ src/JS/index.js
@@ -1,10 +1,51 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-
-
-
-
-
+import { fetchBreeds } from './cat-api';
+import { fetchCatByBreed } from './cat-api';
 const breedSelect = document.querySelector('.breed-select');
 const pLoader = document.querySelector('.loader');
 const catInfo = document.querySelector('.cat-info');
+pLoader.style.display = "none"
+
+
+fetchBreeds().then(data => {
+    const formListEl = document.createElement("option");
+    formListEl.value = "";
+    formListEl.textContent = "Select a cat breed";
+    breedSelect.appendChild(formListEl);
+
+
+    data.forEach(breed => {
+        const list = document.createElement("option");
+        list.value = breed.id;
+        list.textContent = breed.name;
+        list.placeholder = "Select breed";
+        breedSelect.appendChild(list);
+    });
+
+}).catch(err => console.log(err));
+
+
+function createMarkup(data) {
+    const imageEl = data[0].url;
+    const markup = data[0].breeds.map((breed) => {
+        return `<img class="breed-image" alt=${breed.name} src=${imageEl}>
+    <div class="breed-container">
+    <h2 class="breed-name"> ${breed.name}</h2>
+    <p class="breed-temperament"> ${breed.temperament}</p>
+    <p class="breed-description"> ${breed.description}</p>
+</div>`;
+    }).join("");
+
+    catInfo.innerHTML = markup;
+    pLoader.style.display = "none"
+}
+
+
+const onClick = evt => {
+    const breedId = evt.target.value;
+    breedSelect.value = ""
+    pLoader.style.display = "block"
+    fetchCatByBreed(breedId).then(data => createMarkup(data)).catch(err => console.log(err));
+}
+const catById = breedSelect.addEventListener("change", onClick)
+
