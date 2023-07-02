e77b62ed2a25bc0c13ba70e794afe46443798631
Iryna220 <ibiriszka951@gmail.com>
2023-07-02 15:49:55 +0200

    fixing hw10

--- src/JS/cat-api.js
+++ src/JS/cat-api.js
@@ -5,22 +5,24 @@ const BASE_URL = "https://api.thecatapi.com/v1/";
 const API_KEY = "live_u21Y5Igzf6bc5bGcRoQ2WJn4RVCcVNRckcdQ4GPdVUddRpaapl6Wd4Ffnyg1db3t";
 
 
-export const notifyFailure = (errorMessage) => {
-  Notify.failure(errorMessage, {
-    timeout: 6000,
-    width: '550px',
-    borderRadius: '50px',
-    clickToClose: true,
-    position: 'center-top',
-    fontSize: '18px',
-  });
-};
+export const options  = {
+  
+  width: '555px',
+  fontSize: '18px',
+  position: 'center-bottom',
+  distance: '33px',
+  borderRadius: '13px',
+  timeout: 2200,
+  clickToClose: true,
+  cssAnimationStyle: 'from-right',
+  };
+
 
 export const fetchBreeds = function() {
     return fetch(`${BASE_URL}breeds?api_key=${API_KEY}`)
         .then(response => {
         if (!response.ok) {
-         notifyFailure("Oops! Something went wrong! Try reloading the page!")
+         Notify.failure("Oops! Something went wrong! Try reloading the page!", options)
         }
         return response.json()
  })   
@@ -30,7 +32,7 @@ export const fetchCatByBreed = function(breedId) {
     return fetch(`${BASE_URL}images/search?breed_ids=${breedId}&api_key=${API_KEY}`)
         .then(response => {
             if (!response.ok) {
-                notifyFailure("Oops! Something went wrong! Try reloading the page!")
+                Notify.failure("Oops! Something went wrong! Try reloading the page!", options)
             }
             return response.json()
         })
